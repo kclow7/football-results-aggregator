abdc0eb888bf71b94ead409c172cf743f6d2bbfd
kianchong <kianchonglow314@gmail.com>
2019-11-28 23:11:08 +0800

    Added links to the buttons representing each league

--- config/routes.rb
+++ config/routes.rb
@@ -6,18 +6,18 @@ Rails.application.routes.draw do
 
   root "pages#home"
 
-  get "premier_league", to: "pages#premier_league"
+  get "premier_league", to: "pages#premier_league", as: :premier_league
   get "premier_league/:matchday", to: "pages#premier_league"
 
-  get "la_liga", to: "pages#la_liga"
+  get "la_liga", to: "pages#la_liga", as: :la_liga
   get "la_liga/:matchday", to: "pages#la_liga"
 
-  get "ligue_1", to: "pages#ligue_1"
+  get "ligue_1", to: "pages#ligue_1", as: :ligue_1
   get "ligue_1/:matchday", to: "pages#ligue_1"
 
-  get "bundesliga", to: "pages#bundesliga"
+  get "bundesliga", to: "pages#bundesliga", as: :bundesliga
   get "bundesliga/:matchday", to: "pages#bundesliga"
 
-  get "serie_a", to: "pages#serie_a"
+  get "serie_a", to: "pages#serie_a", as: :serie_a
   get "serie_a/:matchday", to: "pages#serie_a"
 end
