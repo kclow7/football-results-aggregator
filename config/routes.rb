d8eab0cd97cab9d06fadd4e0e98376ab6834a2a6
kianchong <kianchonglow314@gmail.com>
2019-12-04 19:44:10 +0800

    blocked out resource routes like show, index, edit, etc

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  resources :teams
-  resources :matches
-  resources :leagues
+  # resources :teams
+  # resources :matches
+  # resources :leagues
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root "pages#home", as: :home
